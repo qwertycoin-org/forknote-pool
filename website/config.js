dbd41aa9408f0fdf1644174938fb78589cbf62dd
orangecoi <35841252+orangecoi@users.noreply.github.com>
2018-03-04 20:55:12 +0100

    Update Default Config

--- website/config.js
+++ website/config.js
@@ -20,6 +20,14 @@ var themeCss = "themes/default-theme.css";
 
 var networkStat = {
     "qwc": [
-        ["qwertycoin.site", "https://qwertycoin.site:8119"]
+        ["qwertycoin.site", "https://qwertycoin.site:8119"],
+        ["pool.ahka.net/qwertycoin", "http://qwc.pool.ahka.net:8116"],
+        ["qwerty.poolminer.work", "http://qwerty.poolminer.work:8117"],
+        ["wc.cnotepool.info", "http://qwc.cnotepool.info:8117"],
+        ["cryptoknight.cc/qwerty", "http://78.46.85.142:6559"],
+        ["superblockchain.con-ip.com", "http://superblockchain.con-ip.com:8333"],
+        ["pool.mineallcrypto.com", "http://pool.mineallcrypto.com:6076"],
+        ["qwertycoin.poolmining.it", "http://qwertycoin.poolmining.it:8117"],
+        ["pool-marine.ga/qwc", "http://pool-marine.ga:8113"]
     ]
 };
